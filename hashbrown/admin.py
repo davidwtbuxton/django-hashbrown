6e9d2f30ac1464450ffb2356f2517df41b7fe76b
David Buxton <david@gasmark6.com>
2014-11-06 21:29:18 +0000

    Show active users for each switch in the admin list view.

--- hashbrown/admin.py
+++ hashbrown/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
+from django.utils.encoding import force_text
 
 from .models import Switch
 
 
 class SwitchAdmin(admin.ModelAdmin):
-    list_display = ('label', 'globally_active', 'description')
+    list_display = ('label', 'globally_active', 'active_for_users', 'description')
     list_editable = ('globally_active',)
+
+    def active_for_users(self, obj):
+        return u', '.join(force_text(user) for user in obj.users.all())
 
 
 admin.site.register(Switch, SwitchAdmin)
